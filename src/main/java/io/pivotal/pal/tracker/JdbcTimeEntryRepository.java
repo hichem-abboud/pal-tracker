4007e08b55c2b155e3be35341b647ccee7dc3e21
hichem-abboud <hichem.abboud@gmail.com>
2019-11-23 18:02:11 +0100

    jdbc lab

--- src/main/java/io/pivotal/pal/tracker/JdbcTimeEntryRepository.java
+++ src/main/java/io/pivotal/pal/tracker/JdbcTimeEntryRepository.java
@@ -1,4 +1,73 @@
 package io.pivotal.pal.tracker;
 
-public class JdbcTimeEntryRepository {
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.ResultSetExtractor;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
+
+import javax.sql.DataSource;
+import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.util.List;
+
+import static java.sql.Statement.RETURN_GENERATED_KEYS;
+
+public class JdbcTimeEntryRepository implements TimeEntryRepository {
+
+    private JdbcTemplate jdbcTemplate;
+    KeyHolder generatedKeyHolder = new GeneratedKeyHolder();
+    public JdbcTimeEntryRepository(DataSource dataSource) {
+        this.jdbcTemplate = new JdbcTemplate(dataSource);
+    }
+
+    @Override
+    public TimeEntry create(TimeEntry timeEntry) {
+        jdbcTemplate.update(con -> {
+            PreparedStatement statement = con.prepareStatement("INSERT INTO time_entries (project_id, user_id, date, hours) " +
+                    "VALUES (?, ?, ?, ?)",RETURN_GENERATED_KEYS);
+            statement.setLong(1, timeEntry.getProjectId());
+            statement.setLong(2, timeEntry.getUserId());
+            statement.setDate(3, Date.valueOf(timeEntry.getDate()));
+            statement.setInt(4, timeEntry.getHours());
+            return statement;
+        },generatedKeyHolder);
+        return find(generatedKeyHolder.getKey().longValue());
+    }
+
+    @Override
+    public TimeEntry find(long timeEntryId) {
+        TimeEntry timeEntry = jdbcTemplate.query("SELECT id, project_id, user_id, date, hours FROM time_entries WHERE id = ?",new Object[]{timeEntryId},resultSetExtractor);
+        return timeEntry;
+    }
+    RowMapper<TimeEntry> mapper = (rs, rowNum) -> {
+        TimeEntry timeEntry = new TimeEntry(rs.getLong("id"), rs.getLong("project_id"), rs.getLong("user_id"), rs.getDate("date").toLocalDate(), rs.getInt("hours"));
+        return timeEntry;
+    };
+
+    ResultSetExtractor<TimeEntry> resultSetExtractor = rs -> rs.next()?mapper.mapRow(rs,1):null;
+    @Override
+    public List<TimeEntry> list() {
+       return jdbcTemplate.query("SELECT id, project_id, user_id, date, hours FROM time_entries",mapper);
+    }
+
+    @Override
+    public TimeEntry update(long timeEntryId, TimeEntry timeEntry) {
+        jdbcTemplate.update("UPDATE time_entries " +
+                        "SET project_id = ?, user_id = ?, date = ?,  hours = ? " +
+                        "WHERE id = ?",
+                timeEntry.getProjectId(),
+                timeEntry.getUserId(),
+                Date.valueOf(timeEntry.getDate()),
+                timeEntry.getHours(),
+                timeEntryId);
+
+        return find(timeEntryId);
+    }
+
+    @Override
+    public void delete(long timeEntryId) {
+        jdbcTemplate.update("DELETE FROM time_entries WHERE id = ?", timeEntryId);
+
+    }
 }
--- src/main/java/io/pivotal/pal/tracker/TimeEntryController.java
+++ src/main/java/io/pivotal/pal/tracker/TimeEntryController.java
@@ -1,4 +1,58 @@
 package io.pivotal.pal.tracker;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+@RestController
+@RequestMapping("/time-entries")
 public class TimeEntryController {
+
+    private TimeEntryRepository timeEntryRepository;
+
+    public TimeEntryController(TimeEntryRepository timeEntryRepository) {
+        this.timeEntryRepository = timeEntryRepository;
+    }
+    @PostMapping
+    public ResponseEntity create(@RequestBody TimeEntry timeEntryToCreate) {
+        TimeEntry timeEntry = timeEntryRepository.create(timeEntryToCreate);
+        ResponseEntity responseEntity = new ResponseEntity(timeEntry,HttpStatus.CREATED);
+        return responseEntity;
+    }
+    @GetMapping("{timeEntryId}")
+    public ResponseEntity<TimeEntry> read(@PathVariable long timeEntryId) {
+        TimeEntry timeEntry = timeEntryRepository.find(timeEntryId);
+        if(timeEntry!=null){
+            return new ResponseEntity(timeEntry,HttpStatus.OK);
+        }else{
+            return new ResponseEntity(HttpStatus.NOT_FOUND);
+        }
+
+    }
+    @GetMapping
+    public ResponseEntity<List<TimeEntry>> list() {
+        List<TimeEntry> list =  timeEntryRepository.list();
+        return new ResponseEntity(list,HttpStatus.OK);
+    }
+    @PutMapping("{timeEntryId}")
+    public ResponseEntity update(@PathVariable long timeEntryId, @RequestBody TimeEntry expected) {
+        TimeEntry timeEntry = timeEntryRepository.update(timeEntryId,expected);
+        if(timeEntry!=null){
+            return new ResponseEntity(timeEntry,HttpStatus.OK);
+        }else{
+            return new ResponseEntity(HttpStatus.NOT_FOUND);
+        }
+    }
+    @DeleteMapping("{timeEntryId}")
+    public ResponseEntity delete(@PathVariable long timeEntryId) {
+        if(timeEntryRepository.find(timeEntryId)!=null){
+            timeEntryRepository.delete(timeEntryId);
+            return new ResponseEntity(HttpStatus.NO_CONTENT);
+        }else{
+            return new ResponseEntity(HttpStatus.NOT_FOUND);
+        }
+
+    }
 }
